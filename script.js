134f4ff9204cf6e70a9f9f985bef9bb85d4bb52c
ececere1327 <evc4@njit.edu>
2024-11-19 16:17:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // I am not all too familiar with Java or JavaScript so I watched a youtube tutorial on how to create a cookie
 
 function setCookie() {
-    const d = new Date();
-    d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000)); // Cookie expires in 7 days
-    const expires = "expires=" + d.toUTCString();
-    document.cookie = "username=User; " + expires + "; path=/";
-    displayCookie();
+    const username = prompt("Please enter your name:");
+    if (username != null && username != "") {
+        const d = new Date();
+        d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000)); 
+        const expires = "expires=" + d.toUTCString();
+        document.cookie = "username=" + username + "; " + expires + "; path=/";
+        displayCookie();
+    }
 }
+
 
 function getCookie(name) {
     const cname = name + "=";
@@ -24,6 +28,7 @@ function getCookie(name) {
     return "";
 }
 
+
 function displayCookie() {
     const user = getCookie("username");
     if (user !== "") {
@@ -32,6 +37,7 @@ function displayCookie() {
         document.getElementById("cookieDisplay").textContent = "No cookie set. Click the button to set a cookie.";
     }
 }
+
 
 window.onload = function() {
     displayCookie();
